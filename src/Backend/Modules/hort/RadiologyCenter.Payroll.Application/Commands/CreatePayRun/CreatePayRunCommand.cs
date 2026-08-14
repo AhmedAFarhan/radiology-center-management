ab0f6320faff8cb0e8d44710c83589af8230a009
Ahmed Abdou <Ahmed Abdou@DESKTOP-PL8IF8N>
2026-08-14 05:10:46 +0300

    enahnce code quality

--- src/Backend/Modules/hort/RadiologyCenter.Payroll.Application/Commands/CreatePayRun/CreatePayRunCommand.cs
+++
@@ -1,6 +0,0 @@
-namespace RadiologyCenter.Payroll.Application.Commands.CreatePayRun;
-
-public record CreatePayRunCommand(
-    DateTime RunFrom,
-    DateTime RunTo,
-    string? Notes = null) : ICommand;
